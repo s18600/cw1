9e23b8c23b4c27c55098100d22d7cc15c5c32af4
Oskar <lol199pl@gmail.com>
2020-10-30 02:05:05 +0100

    dodatkowe-wymagania

--- Cw1/Program.cs
+++ Cw1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -9,26 +10,52 @@ namespace Cw1
     {
         public static async Task Main(string[] args)
         {
-            if (args.Length > 0)
-            {   
-                var httpClient = new HttpClient();
-                var response = await httpClient.GetAsync(args[0]);
-             
-                if (response.IsSuccessStatusCode)
+            try
+            {
+                if(args.Length>0)
                 {
-                    var html = await response.Content.ReadAsStringAsync();
-                    var regex = new Regex("[a-zA-Z0-9]+@[a-z]+[.][a-z]+");
-
-                    MatchCollection matches = regex.Matches(html);
-                    foreach(var i in matches)
+                    try
                     {
-                        Console.WriteLine(i);
+                        var httpClient = new HttpClient();
+                        var response = await httpClient.GetAsync(args[0]);
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var html = await response.Content.ReadAsStringAsync();
+                            httpClient.Dispose();
+                            var regex = new Regex("[a-zA-Z0-9]+@[a-z]+[.][a-z]+");
+
+                            MatchCollection matches = regex.Matches(html);
+                            var uniqueMatches = matches.OfType<Match>().Select(m => m.Value).Distinct();
+                            if (matches.Count==0)
+                            {
+                                Console.WriteLine("Nie znaleziono adresów email");
+                            }
+                            foreach (var i in uniqueMatches)
+                            {
+                                Console.WriteLine(i);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Bład wczasie pobierania strony");
+                        }
+
                     }
+                    catch (System.InvalidOperationException)
+                    {
+                        throw new ArgumentException();
+                    }            
 
-                    
                 }
-
+                else
+                {
+                    throw new ArgumentNullException(paramName: "args",message: "No arg");
+                }
             }
+            catch (Exception) { }
+                
+                        
+                       
             Console.WriteLine("end");
         }
     }
